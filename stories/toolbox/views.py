ae6e70a60678e6252794d27d1b7921509fdb372e
Alexander Valet <alexander.valet@edelight.de>
2012-07-19 18:50:21 +0200

    Add goals and stories to template

--- stories/toolbox/views.py
+++ stories/toolbox/views.py
@@ -2,11 +2,13 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from toolbox.models import Project
+from toolbox.models import Project, Goal, Story
 
 
 def main(request):
     """Default main view"""
-    data = {'projects': Project.objects.all()}
+    data = {'projects': Project.objects.all(), 
+            'goals': Goal.objects.all(),
+            'stories': Story.objects.all()}
     return render_to_response('base.html',
                               data, context_instance=RequestContext(request))
